a7d6a7bb9d7703b00a2e3ad8d2632f572f45856e
John Bellardo <bellardo@calpoly.edu>
2017-10-28 12:13:14 -0700

    Updates for new Intrepid 1.5 bootloader

--- boot/polysatbootstrap/polysatbootstrap.mk
+++ boot/polysatbootstrap/polysatbootstrap.mk
@@ -33,7 +33,7 @@ endef
 define POLYSATBOOTSTRAP_INSTALL_HOOK
 	mv $(2)/$(1).$(ROOTFS_FILE_NAME) $(2)/rootfs.bin
 	mv $(2)/$(1).$(USRLOCAL_FILE_NAME) $(2)/$(USRLOCAL_FILE_NAME)
-	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) DFL_KERN=$(2)/linux-kernel.bin DFL_FS=$(2)/rootfs.bin DFL_LOCAL_FS=$(2)/$(USRLOCAL_FILE_NAME) -C $(POLYSATBOOTSTRAP_DIR)
+	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) DFL_IMAGE_DIR=$(2) DFL_KERN=linux-kernel.bin DFL_FS=rootfs.bin DFL_LOCAL_FS=$(USRLOCAL_FILE_NAME) -C $(POLYSATBOOTSTRAP_DIR)
 	cp -f $(POLYSATBOOTSTRAP_DIR)/install.sh $(2)
 	cp $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-part2.bin $(2)/bootstrap-part2.bin
 	cp $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-devboard.bin $(2)/bootstrap-devboard-64mb.bin
@@ -45,6 +45,7 @@ define POLYSATBOOTSTRAP_INSTALL_HOOK
 	cp $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-64-4nand-flightboard.bin $(2)/bootstrap-intrepid-64mb-4nand.bin
 	cp $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-flightboard.bin $(2)/bootstrap-intrepid-128mb.bin
 	cp $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-flightboard.bin $(2)/bootstrap-intrepid-128mb.bin
+	cp $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-intrepid1.5.bin $(2)/bootstrap-intrepid1.5.bin
 	rm -f $(2)/.sed_script
 	echo s!set basePath \".*\"!set basePath \"`cd $(2); pwd -P `\"!g >> $(2)/.sed_script
 	echo s!asePath/.*/linux-kernel.bin\"!asePath/linux-kernel.bin\"!g >> $(2)/.sed_script
@@ -52,8 +53,11 @@ define POLYSATBOOTSTRAP_INSTALL_HOOK
 	echo s/intrepid_at91sam9g20ek-part2.bin/bootstrap-part2.bin/g >> $(2)/.sed_script
 	echo s/intrepid_at91sam9g20ek-devboard.bin/bootstrap-devboard-64mb.bin/g >> $(2)/.sed_script
 	echo s/intrepid_at91sam9g20ek-64-flightboard.bin/bootstrap-intrepid-64mb.bin/g >> $(2)/.sed_script
+	echo s/intrepid_at91sam9g20ek-intrepid1.5/bootstrap-intrepid1.5/g >> $(2)/.sed_script
 	sed -f $(2)/.sed_script $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-DevBoard-AT45DB.tcl > $(2)/flash-devboard-64mb.tcl
 	sed -f $(2)/.sed_script $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-intrepid-64.tcl > $(2)/flash-intrepid-64mb.tcl
+	sed -f $(2)/.sed_script $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-intrepid-64.tcl > $(2)/flash-intrepid-64mb.tcl
+	sed -f $(2)/.sed_script $(POLYSATBOOTSTRAP_DIR)/intrepid_at91sam9g20ek-intrepid1.5.tcl > $(2)/flash-intrepid1.5.tcl
 	sed -e s/bootstrap-intrepid-64mb/bootstrap-intrepid-128mb/g $(2)/flash-intrepid-64mb.tcl > $(2)/flash-intrepid-128mb.tcl
 	sed -e s/bootstrap-intrepid-64mb/bootstrap-intrepid-128mb-4nand/g $(2)/flash-intrepid-64mb.tcl > $(2)/flash-intrepid-128mb-4nand.tcl
 	sed -e s/bootstrap-intrepid-64mb/bootstrap-intrepid-64mb-4nand/g $(2)/flash-intrepid-64mb.tcl > $(2)/flash-intrepid-64mb-4nand.tcl
@@ -72,6 +76,7 @@ define POLYSATBOOTSTRAP_INSTALL_HOOK
 	sed -e s/bootstrap-devboard-64mb/bootstrap-devboard-64mb-4nand/g $(2)/flash-devboard-64mb.sh > $(2)/flash-devboard-64mb-4nand.sh
 	sed -e s/bootstrap-devboard-64mb/bootstrap-devboard-128mb-4nand/g $(2)/flash-devboard-64mb.sh > $(2)/flash-devboard-128mb-4nand.sh
 	sed -e s/bootstrap-devboard-64mb/bootstrap-devboard-128mb/g $(2)/flash-devboard-64mb.sh > $(2)/flash-devboard-128mb.sh
+	#sed -e s/bootstrap-devboard-64mb/bootstrap-devboard-128mb/g $(2)/intrepid_at91sam9g20ek-intrepid1.5.sh > $(2)/flash-devboard-128mb.sh
 	rm $(2)/.sed_script
 endef
 

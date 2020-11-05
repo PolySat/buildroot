356a6c8f79b6b835205dd313cc329853d7007bf1
John Bellardo <bellardo@calpoly.edu>
2020-11-04 16:42:53 -0800

    Add epeg support into buildroot

---
+++ package/epeg/epeg.mk
@@ -0,0 +1,18 @@
+#############################################################
+#
+# epeg
+#
+#############################################################
+EPEG_VERSION = 248ae9f
+EPEG_SITE = https://github.com/mattes/epeg.git
+EPEG_SITE_METHOD=git
+EPEG_INSTALL_STAGING = YES
+EPEG_DEPENDENCIES = jpeg libexif
+
+define EPEG_CREATE_CONFIG_FILE
+   (cd $(@D); PATH=$(PATH):$(HOST_DIR)/usr/bin NOCONFIGURE=1 $(@D)/autogen.sh)
+endef
+
+EPEG_PRE_CONFIGURE_HOOKS += EPEG_CREATE_CONFIG_FILE
+
+$(eval $(call AUTOTARGETS))
